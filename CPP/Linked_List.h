65303ef58c8549bab241ccaa72bcef2f5d50ed2b
M. Moccaro <mfm2772@comcast.net>
2017-02-02 21:23:15 -0600

    Update Linked_List.h

--- CPP/Linked_List.h
+++ CPP/Linked_List.h
@@ -34,42 +34,94 @@ class Linked_List
 		//Copy Constructor
 		Linked_List(const Linked_List &list)
 		{
+			//Set current to head node
+			current = list.head;
+
+			//Create head node
+			head = new Linked_List_Node<T>;
+			head->set_data(current->get_data());
+			temp = head;	
+
+			current = current->get_next();		
+
+			//Copy the remainder of the list
+			while(current != NULL)
+			{		
+				temp->set_next(new Linked_List_Node<T>);
+				temp = temp->get_next();		 
+				temp->set_data(current->get_data());
+				current = current->get_next();
+			}
+			temp->set_next(NULL);
 		}
 
 		//Destructor
 		~Linked_List()
 		{
-		}
-
-		//Initialization
-		void initialize()
-		{
+			//Iterate through the list and clean up memory
+			current = head;
+			while(current != NULL)
+			{
+				current = head->get_next();
+				delete head;
+				head = current;
+			}
 		}
 
 		//Methods
 		void add_front(T node_data)
 		{
+			//Set current to head temporarily
 			current = head;
+
+			//Create a new node and set its parameters
 			head = new Linked_List_Node<T>;
 			head->set_data(node_data);
 			head->set_next(current);
+			current = head;
 		}
 
 		//Add Back
-		void add_back(Linked_List_Node<T> node)
+		void add_back(T node_data)
 		{
+			current = head;
+			while(current->get_next() != NULL)
+			{
+				current = current->get_next();
+			}
+			current->set_next(new Linked_List_Node<T>);
+			current = current->get_next();
+			current->set_data(node_data);
+			current->set_next(NULL);
 		}
 
 		//Remove Front
-		Linked_List_Node<T> remove_front()
+		T remove_front()
 		{
-			return NULL;
+			current = head;
+			head = head->get_next();
+
+			current->set_next(NULL);
+			delete current;
+			current = head;
+
+			return 1;
 		}
 
 		//Remove Back
-		Linked_List_Node<T> remove_back()
+		T remove_back()
 		{
-			return NULL;
+			current = head->get_next();
+			temp = head;
+			while(current->get_next() != NULL)
+			{
+				current = current->get_next();
+				temp = temp->get_next();
+			}
+			T data = current->get_data();
+			delete current;
+			temp->set_next(NULL);
+			return data;			
 		}
 
 		//Print
@@ -87,5 +139,6 @@ class Linked_List
 		Linked_List_Node<T> *head;
 		Linked_List_Node<T> *tail;
 		Linked_List_Node<T> *current;
+		Linked_List_Node<T> *temp;
 };
 #endif
