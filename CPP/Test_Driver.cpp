441be26a6a899134e353bc0efdee73b3ffce3f3f
M. Moccaro <mfm2772@comcast.net>
2017-02-02 21:29:18 -0600

    Update Test_Driver.cpp

--- CPP/Test_Driver.cpp
+++ CPP/Test_Driver.cpp
@@ -24,7 +24,25 @@ int main()
 	Linked_List<int> list;
 	list.add_front(7);
 	list.add_front(5);
+	list.add_front(53);
+	list.add_front(25);
+	list.add_front(98);
+	list.add_front(1);
+	list.add_front(34);
 	list.print();
+
+	cout << "LIST 2-------------" << endl;
+	Linked_List<int> list2(list);
+	list2.print();
+	cout << "Adding 78 to back..." << endl;
+	list2.add_back(78);
+	list2.print();
+	cout << "Removing 78 from back..." << endl;
+	int data = list2.remove_back();
+	list2.print();
+	cout << "Removing from front..." << endl;
+	data = list2.remove_front();
+	list2.print();
 	
 
 	//Exit Code
