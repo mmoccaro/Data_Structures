c041296f57600ef8d1620efa0ee6b6a236ea0dfc
M. Moccaro <mfm2772@comcast.net>
2017-02-09 15:39:40 -0600

    Update Linked_List.h

--- CPP/Linked_List.h
+++ CPP/Linked_List.h
@@ -29,13 +29,24 @@ template <class T>
 class Linked_List
 {
 	public:
-		//Constructor
+		//Constructors
 		Linked_List()
 		{
-			head = tail = current = NULL;
-			type = SINGLY_LINKED_LIST;
-			dummy_head_node = false;
-			circular_list = false;
+			head = tail = current = temp = NULL;
+			this->type = SINGLY_LINKED_LIST;
+			this->dummy_head_node = false;
+			this->circular_list = false;
+			this->size = 0;
+		}
+
+		//Input Type Constructor
+		Linked_List(int type, bool dummy_head_node, bool circular_list)
+		{
+			head = tail = current = temp = NULL;
+			this->type = type;
+			this->dummy_head_node = dummy_head_node;
+			this->circular_list = circular_list;
+			this->size = 0;
 		}
 
 		//Copy Constructor
@@ -43,6 +54,9 @@ class Linked_List
 		{
 			//Set current to head node
 			current = list.head;
+			type = list.type;
+			dummy_head_node = list.dummy_head_node;
+			circular_list = list.circular_list;
 
 			//Create head node
 			try
@@ -55,7 +69,8 @@ class Linked_List
 			}
 
 			head->set_data(current->get_data());
-			temp = head;	
+			temp = head;
+			size = 1;	
 
 			current = current->get_next();		
 
@@ -73,8 +88,10 @@ class Linked_List
 				temp = temp->get_next();		 
 				temp->set_data(current->get_data());
 				current = current->get_next();
+				size++;
 			}
 			temp->set_next(NULL);
+			tail = temp;
 		}
 
 		//Destructor
@@ -105,20 +122,35 @@ class Linked_List
 			{
 				cerr << "Memory allocation failed." << endl;
 			}
+
+			//Set parameters of new node
 			head->set_data(node_data);
 			head->set_next(current);
 			current = head;
+
+			//Set Tail
+			if(size == 0)
+			{
+				tail = current;
+			}
+
+			//Increment Size
+			size++;
 		}
 
 		//Add Back
 		void add_back(T node_data)
 		{
+			//Assign current pointer
 			current = head;
+
+			//Find the end of the list
 			while(current->get_next() != NULL)
 			{
 				current = current->get_next();
 			}
 
+			//Allocate a new node
 			try
 			{
 				current->set_next(new Linked_List_Node<T>);
@@ -127,9 +159,15 @@ class Linked_List
 			{
 				cerr << "Memory allocation failed." << endl;
 			}
+
+			//Set Parameters of new node
 			current = current->get_next();
 			current->set_data(node_data);
 			current->set_next(NULL);
+			tail = current;
+
+			//Increment Size
+			size++;
 		}
 
 		//Remove Front
@@ -142,30 +180,52 @@ class Linked_List
 			delete current;
 			current = head;
 
+			//Decrement Size
+			size--;
+
 			return data;
 		}
 
 		//Remove Back
 		T remove_back()
 		{
+			//Offset current and temp pointers
 			current = head->get_next();
 			temp = head;
+
+			//Iterate through to the end of the list.
 			while(current->get_next() != NULL)
 			{
 				current = current->get_next();
 				temp = temp->get_next();
 			}
+
+			//Get the data and then delete the node
 			T data = current->get_data();
 			delete current;
+
+			//Set the new tail
 			temp->set_next(NULL);
+			tail = temp;
+
+			//Decrement Size
+			size--;
+
+			//Return the deleted node's data
 			return data;			
 		}
 
 		//Print
 		void print()
 		{
+			//Set iterator pointer
 			current = head;
-			while(current != NULL)
+
+			//Set the size
+			cout << "Size: " << size << endl;
+			
+			//Iterate through list and print each node's data.
+			while(current != tail->get_next())
 			{
 				cout << current->get_data() << endl;
 				current = current->get_next();
@@ -184,5 +244,8 @@ class Linked_List
 		Type type;
 		bool dummy_head_node;
 		bool circular_list;
+
+		//Attributes
+		int size;
 };
 #endif
